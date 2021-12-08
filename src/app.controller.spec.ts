be456eb324c8e4ea4f0a64a65e57370dbf0c2252
Emily Lino <linoemily99@gmail.com>
2021-12-07 19:50:45 -0500

    probando..

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello TSR!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
