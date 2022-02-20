3660674d0be169d4b903e98e70207e3bdc42f864
Emily Lino <linoemily99@gmail.com>
2022-02-20 00:33:03 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api-ppg/v1/');
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
