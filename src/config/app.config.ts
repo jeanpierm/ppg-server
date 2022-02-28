045947dc2529d9c24f413512a3c39d422a75c8f1
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2022-02-28 18:38:48 -0500

    PORT config var

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -23,8 +23,9 @@ export default registerAs(
     timeout: parseInt(process.env.APP_TIMEOUT_IN_MS) || 90_000,
     language: process.env.APP_LANGUAGE || 'es',
     http: {
-      host: process.env.APP_HOST || '0.0.0.0',
-      port: parseInt(process.env.APP_PORT) || 3000,
+      host: process.env.HOST || process.env.APP_HOST || '0.0.0.0',
+      port:
+        parseInt(process.env.PORT) || parseInt(process.env.APP_PORT) || 3000,
     },
     timezone: process.env.APP_TZ || 'America/Guayaquil',
   }),
