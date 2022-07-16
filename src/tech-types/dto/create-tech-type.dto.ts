e50025bb7e5bc289ef477be2af7f1c06576b230b
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2022-07-15 19:12:12 -0500

    doc: add create-tech-type openapi doc

--- src/tech-types/dto/create-tech-type.dto.ts
+++ src/tech-types/dto/create-tech-type.dto.ts
@@ -1,10 +1,18 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTechTypeDto {
+  /**
+   * El nombre que identifica al tipo de tecnología
+   * @example language
+   */
   @IsString()
   @IsNotEmpty()
   readonly name: string;
 
+  /**
+   * Un label que puede ser usado como título
+   * @example Lenguajes
+   */
   @IsString()
   @IsNotEmpty()
   readonly label: string;
